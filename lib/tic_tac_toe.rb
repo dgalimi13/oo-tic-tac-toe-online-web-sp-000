fd0ff31d590f0b1b69f131c196453e57fc232752
danny galimi <dannygalimi13@aim.com>
2020-09-29 19:42:15 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -22,8 +22,8 @@ def move(index,character = "X")
   @board[index] = character
 end
   
-  def position_taken?(board,index)
-    @board[index] == "X" || @board[index] == "O"
-  end
+def position_taken?(board,index)
+board[index] == "X" || board[index] == "O"
+end
   
 end 
