38ff71dc86285101824fcf08f60e1ff87bfe9917
danny galimi <dannygalimi13@aim.com>
2020-09-29 19:58:10 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -66,7 +66,8 @@ def turn
 end 
 
 def won?
-  
+  WIN_COMBINATIONS.find do |win_combination|
+  @board[win_combination[0]] == @board[win_combination[1]] && @board[win_combination[1]] == @board[win_combination[2]] && position_taken?(index)
 end 
   
   
