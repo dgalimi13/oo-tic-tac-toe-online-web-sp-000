d5ae2ea8abaead9e49ffa991cd517474bad48a44
danny galimi <dannygalimi13@aim.com>
2020-09-29 19:05:15 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,8 +1,7 @@
 class TicTacToe
   
-  def initialize
-    def initialize(board = nil)
-    @board = board || Array.new(9, " ")
-  end
+def initialize(board = nil)
+   @board = board || Array.new(9, " ")
+end
   
 end 
